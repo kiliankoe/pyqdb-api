2028fb30491f66ad06618476e76452b64b2b7347
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-12-10 17:46:58 +0100

    Getting a new db connection for every request isn't any slower.
    So this should then close #3.

--- server.py
+++ server.py
@@ -5,15 +5,29 @@ import json
 import os
 import hashlib
 
-try:
-    p = Pyqdb(host='127.0.0.1',port=3306,user='',
-              passwd='',db='')
-except pymysql.err.OperationalError as e:
-    print('Error: ' + str(e))
+f = open('secrets.json', 'r')
+secrets = json.loads(f.read())
+f.close()
+
+db_host = secrets['db']['host']
+db_port = int(secrets['db']['port'])
+db_user = secrets['db']['user']
+db_passwd = secrets['db']['pass']
+db_default_db = secrets['db']['db']
 
 
-def check(user, pw):
-    return user == '' and hashlib.md5(pw.encode('utf-8')).hexdigest() == ''
+def connect_to_db():
+    try:
+        p = Pyqdb(host=db_host, port=db_port, user=db_user, passwd=db_passwd, db=db_default_db)
+    except pymysql.OperationalError as e:
+        print('Error: ' + str(e))
+        p = None
+    return p
+
+
+def check(auth_user, auth_pw):
+    return auth_user == secrets['http']['user'] \
+        and hashlib.md5(auth_pw.encode('utf-8')).hexdigest() == secrets['http']['pass']
 
 
 @route('/')
@@ -25,8 +39,11 @@ def get_root():
 @route('/quotes')
 @auth_basic(check)
 def get_quotes():
+    p = connect_to_db()
+
     if not p.test_db():
         return {'Error': 'Database Connection Error'}
+
     response.content_type = 'application/json'
     if 'ip' in request.query:
         results = p.find_by_ip(request.query['ip'])
@@ -51,6 +68,7 @@ def get_quotes():
     if 'before' in request.query:
         results = filter_by_timestamp(request.query['before'], results, 'before')
 
+    p.close()
     # apparently returning a straight list of dicts is unsupported due to security concerns
     # see http://flask.pocoo.org/docs/0.10/security/#json-security
     # but it's not like this tool has sensitive information... :D
@@ -60,24 +78,36 @@ def get_quotes():
 @route('/quotes/<quote_id:int>')
 @auth_basic(check)
 def get_quote_with_id(quote_id):
+    p = connect_to_db()
+
     if not p.test_db():
         return {'Error': 'Database Connection Error'}
+
     response.content_type = 'application/json'
     quote = p.find_by_id(quote_id)
     quote['authors'] = p.process_authors(quote['quote'])
+
+    p.close()
+
     return quote
 
 
 @route('/quotes/lastweek')
 @auth_basic(check)
 def get_last_week():
+    p = connect_to_db()
+
     if not p.test_db():
         return {'Error': 'Database Connection Error'}
+
     ctime = int(time.time()) - 604800
     response.content_type = 'application/json'
     results = filter_by_timestamp(ctime, p.all_quotes())
     for i in range(len(results)):
         results[i]['authors'] = p.process_authors(results[i]['quote'])
+
+    p.close()
+
     return json.dumps(results)
 
 
@@ -91,6 +121,3 @@ if os.getenv('env') == 'development':
     run(reloader=True)
 else:
     run()
-
-
-p.close()
